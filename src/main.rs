44c27bb5a28a5d4138237214a98c7bbd932ac60c
theo <theo@seelye.net>
2021-04-24 13:18:13 -0700

    removed archaic garbage from main

--- src/main.rs
+++ src/main.rs
@@ -8,35 +8,7 @@ use vm1::vm1_raylib;
 use vm1::assembler;
 
 fn main() -> Result<(), String>{
-    // let code1 = vec![
-    //     Instruction::Set {reg: RegAddr {idx: 0}, val: 1},
-    //     Instruction::Inc {reg: RegAddr {idx: 0}, val: 64},
-    //     Instruction::Store {reg: RegAddr {idx: 0}, addr: Addr {addr: 1}},
-    //     Instruction::Set {reg: RegAddr {idx: 1}, val: 31167},
-    //     Instruction::Set {reg: RegAddr {idx: 2}, val: 32767},
-    //     Instruction::StoreDeref {dreg: RegAddr {idx: 0}, areg: RegAddr {idx: 1}},
-    //     Instruction::Inc {reg: RegAddr {idx: 1}, val: 1},
-    //     Instruction::Jlt {a: RegAddr {idx: 1}, b: RegAddr {idx: 2}, addr: CAddr {caddr: 5}},
-    //     Instruction::Draw,
-    // ];
-
-    // let codestr = String::from("set r0 1
-    //                             inc r0 64
-    //                             store r0 1
-    //                             set r1 31167
-    //                             set r2 32767
-    //                             set r4 91
-    //                             store r0 r1
-    //                             inc  r1 1
-    //                             inc r0 1
-    //                             jlt r0 r4 11
-    //                             set r0 65
-    //                             jlt r1 r2 6
-    //                             draw
-    //                             ");
     let args: Vec<String> = env::args().collect();
-
-    println!("{:?}",args);
 
     match args.get(1) {
         None => {
